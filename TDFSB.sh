f35e875750a11660035a34cd3e24660212115b8c
sinxne0 <sinx@GRIM-REAPER-832a2561.tailcfc068.ts.net>
2026-05-02 19:03:08 -0400

    Fix binary paths for TDFSB

--- TDFSB.sh
+++ TDFSB.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# TDFSB Debug Launcher
-exec > /tmp/tdfsb_debug.log 2>&1
-echo "Starting TDFSB at $(date)"
-/usr/bin/tdfsb
-echo "TDFSB exited with code $?"
+# TDFSB Fixed Launcher
+/usr/games/tdfsb
--- XPlanet.sh
+++ XPlanet.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# XPlanet Debug Launcher
-exec > /tmp/xplanet_debug.log 2>&1
-echo "Starting XPlanet at $(date)"
+# XPlanet Fixed Launcher
 OUTPUT="/tmp/xplanet_earth.png"
 /usr/bin/xplanet -num_times 1 -output "$OUTPUT" -geometry 1200x800
-echo "XPlanet exited with code $? (Image saved to $OUTPUT)"
 /usr/bin/xdg-open "$OUTPUT"
-echo "xdg-open exited with code $?"
