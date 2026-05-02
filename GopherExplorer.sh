8890cd7baef83d59841be534b7df7c23067532f1
sinxne0 <sinx@GRIM-REAPER-832a2561.tailcfc068.ts.net>
2026-05-02 18:19:35 -0400

    Revive the Gopher Protocol (1991) - The Lost Internet

---
+++ GopherExplorer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Gopher Explorer
+# Connects to the original text-based 'lost' internet protocol (1991)
+# Uses Lynx as the navigator.
+
+# The Floodgap Gopher server is the primary gateway to the surviving Gopherspace.
+lynx gopher://gopher.floodgap.com
